116ac99916612123f7c6e0464db9ab44d3e963bd
Luis <luisdiegonoar@gmail.com>
2023-07-05 16:22:39 -0500

    presupuesto2

--- src/main/java/com/corazonserrano/FinanzasAPI/model/Presupuesto.java
+++ src/main/java/com/corazonserrano/FinanzasAPI/model/Presupuesto.java
@@ -1,2 +1,29 @@
-package com.corazonserrano.FinanzasAPI.model;public class Presupuesto {
+package com.corazonserrano.FinanzasAPI.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "presupuesto")
+@Data
+public class Presupuesto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "idPresupuesto", updatable = false, nullable = false)
+    private Integer id;
+
+    @Column(name = "login")
+    private Integer login;
+
+    @Column(name = "fechaInicio")
+    private Date fechaInicio;
+
+    @Column(name = "fechaFin")
+    private Date fechaFin;
+
+    @Column(name = "monto")
+    private Double monto;
 }
--- src/main/java/com/corazonserrano/FinanzasAPI/repository/PresupuestoRepository.java
+++ src/main/java/com/corazonserrano/FinanzasAPI/repository/PresupuestoRepository.java
@@ -1,2 +1,8 @@
-package com.corazonserrano.FinanzasAPI.repository;public interface PresupuestoRepository {
+package com.corazonserrano.FinanzasAPI.repository;
+
+import com.corazonserrano.FinanzasAPI.model.Usuario;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PresupuestoRepository {
 }
