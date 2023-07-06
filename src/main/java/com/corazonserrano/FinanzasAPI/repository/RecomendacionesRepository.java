171616f74acd823d91f3868822b19ef1d56098ec
Termita7935 <anbeal789@hotmail.com>
2023-07-06 12:15:34 -0500

    Servicev2

--- src/main/java/com/corazonserrano/FinanzasAPI/repository/RecomendacionesRepository.java
+++ src/main/java/com/corazonserrano/FinanzasAPI/repository/RecomendacionesRepository.java
@@ -3,6 +3,7 @@ package com.corazonserrano.FinanzasAPI.repository;
 import com.corazonserrano.FinanzasAPI.model.Recomendaciones;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface RecomendacionesRepository extends CrudRepository<Recomendaciones, Integer> {
     @Query("SELECT u FROM Recomendaciones u WHERE u.idPresupuesto = :idPresupuesto " +
@@ -12,4 +13,7 @@ public interface RecomendacionesRepository extends CrudRepository<Recomendacione
             "    WHERE idPresupuesto = :idPresupuesto\n" +
             ")")
     Recomendaciones findByidPresupuestoAndnumRecomendacion(Integer idPresupuesto, Integer numRecomendacion);
+
+    @Query(value = "SELECT MAX(r.numRecomendacion) FROM Recomendaciones r WHERE r.idPresupuesto = :idPresupuesto")
+    Integer findMaxNumRecomendacion(@Param("idPresupuesto") Integer idPresupuesto);
 }
--- src/main/java/com/corazonserrano/FinanzasAPI/service/RecomendacionesService.java
+++ src/main/java/com/corazonserrano/FinanzasAPI/service/RecomendacionesService.java
@@ -5,12 +5,43 @@ import com.corazonserrano.FinanzasAPI.repository.RecomendacionesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+
 @Service
 public class RecomendacionesService {
+    private RecomendacionesRepository recomendacionesRepository;
+    private static final int MAX_NUM_RECOMENDACIONES = 4;
+
     @Autowired
-    RecomendacionesRepository recomendacionesRepository;
-    public Recomendaciones obtenerRecomendacion(Integer idPresupuesto, Integer numRecomendacion) {
+    public RecomendacionesService(RecomendacionesRepository recomendacionesRepository) {
+        this.recomendacionesRepository = recomendacionesRepository;
+    }
+
+    public ArrayList<Recomendaciones> obtenerTodasLasRecomendaciones() {
+        return (ArrayList<Recomendaciones>) recomendacionesRepository.findAll();
+    }
+
+    public Recomendaciones obtenerUltimaRecomendacionPorPresupuesto(Integer idPresupuesto) {
+        Integer maxNumRecomendacion = recomendacionesRepository.findMaxNumRecomendacion(idPresupuesto);
+        if (maxNumRecomendacion == null) {
+            return null;
+        }
+        return recomendacionesRepository.findByidPresupuestoAndnumRecomendacion(idPresupuesto, maxNumRecomendacion);
+    }
+
+    public int getMaxNumRecomendacion(Integer idPresupuesto) {
+        Integer maxNumRecomendacion = recomendacionesRepository.findMaxNumRecomendacion(idPresupuesto);
+        if (maxNumRecomendacion == null) {
+            return 0;
+        } else if (maxNumRecomendacion >= MAX_NUM_RECOMENDACIONES) {
+            return MAX_NUM_RECOMENDACIONES;
+        } else {
+            return maxNumRecomendacion;
+        }
+    }
+
+   /* public Recomendaciones obtenerRecomendacion(Integer idPresupuesto, Integer numRecomendacion) {
         Recomendaciones recomendacion = (Recomendaciones) recomendacionesRepository.findByidPresupuestoAndnumRecomendacion(idPresupuesto, numRecomendacion);
         return recomendacion;
-    }
+    }*/
 }
