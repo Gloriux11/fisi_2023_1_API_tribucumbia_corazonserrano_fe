775c00c5800dfabb09cd1ff19a1f3709503a1012
Termita7935 <anbeal789@hotmail.com>
2023-07-06 01:16:40 -0500

    CorregirObtenerRecomendacion

--- src/main/java/com/corazonserrano/FinanzasAPI/controller/RecomendacionesController.java
+++ src/main/java/com/corazonserrano/FinanzasAPI/controller/RecomendacionesController.java
@@ -1,4 +1,24 @@
 package com.corazonserrano.FinanzasAPI.controller;
 
+
+import com.corazonserrano.FinanzasAPI.model.Recomendaciones;
+import com.corazonserrano.FinanzasAPI.service.RecomendacionesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/recomendaciones")
 public class RecomendacionesController {
+    @Autowired
+    RecomendacionesService recomendacionesService;
+    Integer idPresupuesto;
+    Integer numRecomendacion;
+
+    @GetMapping()
+    public ArrayList<Recomendaciones> obtenerRecomendaciones(){
+        return recomendacionesService.obtenerRecomendacion(idPresupuesto, numRecomendacion);
+    }
+
 }
